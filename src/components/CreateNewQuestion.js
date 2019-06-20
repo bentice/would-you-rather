6f95ef118e8bfb6497e3139c966cb801a400f61c
bentice <bentice49@gmail.com>
2019-06-19 20:44:35 -0400

    Shells of Components to Work On

---
+++ src/components/CreateNewQuestion.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+
+class CreateNewQuestion extends Component {
+    render () {
+        return (
+            <div>CreateNewQuestion</div>
+        )
+    }
+}
+
+
+
+
+export default connect()(CreateNewQuestion)
